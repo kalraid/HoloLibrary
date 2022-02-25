bea978e2f97e9933e14ddbc0fabc41824813ecf8
kalraid <kalraid@gmail.com>
2022-02-25 20:52:42 +0900

    [운영] socket 주소 변경

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -1,6 +1,5 @@
 export function connect(data) {
-  const host = process.env.VUE_APP_BASE_API + ':8000'
-  const webSocketUrl = 'ws://' + host + data.url
+  const webSocketUrl = 'ws://' + process.env.VUE_APP_BASE_API + data.url
   const subprotocols = [data.protocol]
   return new WebSocket(webSocketUrl, subprotocols)
 }
