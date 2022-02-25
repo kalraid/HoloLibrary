c66a440258b069a663698e94d650ea91d658b42b
kalraid <kalraid@gmail.com>
2022-02-25 21:06:28 +0900

    [운영] socket http 문구 제거

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -1,5 +1,5 @@
 export function connect(data) {
-  const webSocketUrl = 'ws://' + process.env.VUE_APP_BASE_API + data.url
+  const webSocketUrl = 'ws://' + process.env.VUE_APP_BASE_API_URL + data.url
   const subprotocols = [data.protocol]
   return new WebSocket(webSocketUrl, subprotocols)
 }
